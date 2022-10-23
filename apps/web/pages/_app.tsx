059f99ae48bcaccb60530f7635489b688a3d0b6a
Junho Yeo <i@junho.io>
2022-10-24 02:28:51 +0900

    [web] Collect styles from Geist UI and inject in `DocumentInitialProps`

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { CssBaseline, GeistProvider } from '@geist-ui/core';
 import { type AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 const themeType = 'dark';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    document.querySelector('body')?.classList.remove('preload');
+  }, []);
+
   return (
     <GeistProvider themeType={themeType}>
       <CssBaseline />
