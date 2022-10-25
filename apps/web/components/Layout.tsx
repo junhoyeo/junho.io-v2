a38d56b0fd600b0c80968368e38deb90f3dfe199
Junho Yeo <i@junho.io>
2022-10-25 20:41:40 +0900

    [web] Make left/right content injectable in `Layout`

--- apps/web/components/Layout.tsx
+++ apps/web/components/Layout.tsx
@@ -1,54 +1,38 @@
 import styled from '@emotion/styled';
-import { Breadcrumbs, Card, Page, Text, User, useTheme } from '@geist-ui/core';
+import { Page, useTheme } from '@geist-ui/core';
 
 export interface LayoutProps {
-  header: React.ReactNode;
+  header?: React.ReactNode;
+  leftContent?: React.ReactNode;
+  rightContent?: React.ReactNode;
   children: React.ReactNode;
 }
 
-export const Layout: React.FC<LayoutProps> = ({ header, children }) => {
+export const Layout: React.FC<LayoutProps> = ({
+  header,
+  leftContent,
+  rightContent,
+  children,
+}) => {
   const { palette } = useTheme();
   return (
     <Wrapper>
-      <Sidebar style={{ borderRight: `1px solid ${palette.accents_2}` }} />
+      {!!leftContent && (
+        <Sidebar style={{ borderRight: `1px solid ${palette.accents_2}` }}>
+          {leftContent}
+        </Sidebar>
+      )}
       <Container>
         {header}
         <Page style={{ minHeight: 'unset' }}>
           <div>{children}</div>
         </Page>
       </Container>
-      <Sidebar style={{ borderLeft: `1px solid ${palette.accents_2}` }}>
-        <Breadcrumbs>
-          <Breadcrumbs.Item>Paracosm</Breadcrumbs.Item>
-          <Breadcrumbs.Item>Home</Breadcrumbs.Item>
-        </Breadcrumbs>
-
-        <div
-          style={{
-            marginTop: 16,
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 8,
-          }}
-        >
-          <Card shadow>
-            <User name="Junho Yeo" src="https://github.com/junhoyeo.png">
-              8 minutes ago
-            </User>
-            <Text font="14px" p>
-              Wow
-            </Text>
-          </Card>
-          <Card>
-            <User name="Junho Yeo" src="https://github.com/junhoyeo.png">
-              8 minutes ago
-            </User>
-            <Text font="14px" p>
-              Wow
-            </Text>
-          </Card>
-        </div>
-      </Sidebar>
+      {!!rightContent && (
+        <Sidebar style={{ borderLeft: `1px solid ${palette.accents_2}` }}>
+          {rightContent}
+        </Sidebar>
+      )}
     </Wrapper>
   );
 };
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { Text, useTheme } from '@geist-ui/core';
+import { Breadcrumbs, Card, Text, User, useTheme } from '@geist-ui/core';
 import { Info } from '@geist-ui/icons';
 import { type NextPage } from 'next';
 
@@ -10,7 +10,44 @@ const HomePage: NextPage = () => {
   const { palette } = useTheme();
 
   return (
-    <Layout header={<Header />}>
+    <Layout
+      header={<Header />}
+      leftContent={<div />}
+      rightContent={
+        <>
+          <Breadcrumbs>
+            <Breadcrumbs.Item>Paracosm</Breadcrumbs.Item>
+            <Breadcrumbs.Item>Home</Breadcrumbs.Item>
+          </Breadcrumbs>
+
+          <div
+            style={{
+              marginTop: 16,
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 8,
+            }}
+          >
+            <Card shadow>
+              <User name="Junho Yeo" src="https://github.com/junhoyeo.png">
+                8 minutes ago
+              </User>
+              <Text font="14px" p>
+                Wow
+              </Text>
+            </Card>
+            <Card>
+              <User name="Junho Yeo" src="https://github.com/junhoyeo.png">
+                8 minutes ago
+              </User>
+              <Text font="14px" p>
+                Wow
+              </Text>
+            </Card>
+          </div>
+        </>
+      }
+    >
       <Text blockquote>
         <InfoIcon size={20} />
 
