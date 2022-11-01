278d9955fdb25d3ffc834324ce534ab874f55e99
Junho Yeo <i@junho.io>
2022-11-02 01:24:41 +0900

    [web] Add close button to `CommentDrawer` (w/ same position with open)

--- apps/web/components/CommentDrawer.tsx
+++ apps/web/components/CommentDrawer.tsx
@@ -1,4 +1,6 @@
-import { Breadcrumbs, Drawer } from '@geist-ui/core';
+import styled from '@emotion/styled';
+import { Breadcrumbs, Button, Drawer } from '@geist-ui/core';
+import { X as XIcon } from '@geist-ui/icons';
 import { useAtom, useAtomValue } from 'jotai';
 import React from 'react';
 
@@ -12,16 +14,25 @@ export const CommentDrawer: React.FC = () => {
   const comments = useAtomValue(commentsAtom);
 
   return (
-    <Drawer
+    <StyledDrawer
       onClose={(): void => setCommentDrawerOpen(false)}
       placement="right"
       visible={isCommentDrawerOpen}
     >
       <Drawer.Content style={{ width: 380, paddingTop: 0 }}>
-        <Breadcrumbs>
-          <Breadcrumbs.Item>Paracosm</Breadcrumbs.Item>
-          <Breadcrumbs.Item>Home</Breadcrumbs.Item>
-        </Breadcrumbs>
+        <Header>
+          <Breadcrumbs>
+            <Breadcrumbs.Item>Paracosm</Breadcrumbs.Item>
+            <Breadcrumbs.Item>Home</Breadcrumbs.Item>
+          </Breadcrumbs>
+
+          <Button
+            auto
+            iconRight={<XIcon />}
+            onClick={(): void => setCommentDrawerOpen(false)}
+            px={0.6}
+          />
+        </Header>
 
         <div
           style={{
@@ -36,6 +47,18 @@ export const CommentDrawer: React.FC = () => {
           ))}
         </div>
       </Drawer.Content>
-    </Drawer>
+    </StyledDrawer>
   );
 };
+
+const StyledDrawer = styled(Drawer)`
+  &&& {
+    padding: 16px 32px;
+  }
+`;
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  user-select: none;
+`;
