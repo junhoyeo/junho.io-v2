fdef523927c38f9043d7e1c6c173ab93ad0ef187
Junho Yeo <i@junho.io>
2022-11-02 00:50:35 +0900

    [web] Add empty `leftContent` to `PostPage` layout

--- apps/web/pages/[slug].tsx
+++ apps/web/pages/[slug].tsx
@@ -15,7 +15,7 @@ const components = {};
 
 const PostPage: NextPage<Props> = (props: Props) => {
   return (
-    <Layout>
+    <Layout leftContent={<div />}>
       <Text h1>{props.meta.title}</Text>
       <MDXRemote {...props} components={components} />
     </Layout>
