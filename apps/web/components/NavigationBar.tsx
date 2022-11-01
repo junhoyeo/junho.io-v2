d7d59f162f2cf570a6ff7fbf4d34b7db701f5d5b
Junho Yeo <i@junho.io>
2022-11-02 01:29:49 +0900

    [web] Show brand(icon, project name) in `NavigationBar` and link to root(`/`) (#8)
    * [web] Add brand(icon and project name) to `NavigationBar`
    
    * [web] Link brand to root(`/`)

--- apps/web/components/NavigationBar.tsx
+++ apps/web/components/NavigationBar.tsx
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled';
-import { Button, useTheme } from '@geist-ui/core';
-import { MessageCircle } from '@geist-ui/icons';
+import { Button, Text, useTheme } from '@geist-ui/core';
+import { Anchor, MessageCircle } from '@geist-ui/icons';
 import { useSetAtom } from 'jotai';
+import Link from 'next/link';
 
 import { isCommentDrawerOpenAtom } from '../state/comments';
 
@@ -12,6 +13,15 @@ export const NavigationBar: React.FC = () => {
   return (
     <Wrapper style={{ borderBottom: `1px solid ${palette.accents_1}` }}>
       <Container>
+        <Link href="/">
+          <Brand style={{ color: palette.accents_8 }}>
+            <Anchor size={28} />
+            <Text b span style={{ fontSize: 24 }}>
+              Paracøsm
+            </Text>
+          </Brand>
+        </Link>
+
         <Button
           auto
           iconRight={<MessageCircle />}
@@ -32,11 +42,19 @@ const Wrapper = styled.div`
   z-index: 10;
   background-color: rgba(0, 0, 0, 0.4);
   backdrop-filter: saturate(140%) blur(16px);
+  display: flex;
 `;
 const Container = styled.div`
   width: 100%;
-  padding: 12px;
-
+  padding: 12px 32px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+`;
+
+const Brand = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  user-select: none;
 `;
