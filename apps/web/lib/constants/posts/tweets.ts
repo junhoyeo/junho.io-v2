a1c4bed4bd311bb1b64f54e6dbc402d8bdf9881d
Junho Yeo <i@junho.io>
2022-11-02 13:55:28 +0900

    [web] Add `/lib/constants/*.ts` to `.gitignore`

--- apps/web/lib/constants/posts/tweets.ts
+++
@@ -1,8 +0,0 @@
-import { type PostSummary } from "../../get-posts";
-
-const posts: PostSummary[] = [
-  { title: "Tweets", description: "My Tweets", slug: "" },
-];
-
-// eslint-disable-next-line import/no-default-export
-export default posts;
