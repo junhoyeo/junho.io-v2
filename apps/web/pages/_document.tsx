0063fe4b3d6670f886a84f2ff0d38935451413bf
Junho Yeo <i@junho.io>
2022-12-17 23:50:31 +0900

    [web] Apply color theme fix from https://github.com/chakra-ui/chakra-ui/issues/6192\#issuecomment-1159979257

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import type { ColorMode } from '@chakra-ui/react';
 import { CssBaseline } from '@geist-ui/core';
 import Document, {
   Head,
@@ -12,12 +12,29 @@ import { Fragment } from 'react';
 
 import { theme } from '../styles/theme';
 
-export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext,
-  ): Promise<DocumentInitialProps> {
+type MaybeColorMode = ColorMode | 'system' | undefined;
+
+const parseCookie = (cookie: string, key: string): MaybeColorMode => {
+  const match = new RegExp(`(^| )${key}=([^;]+)`).exec(cookie);
+  return match?.[2] as MaybeColorMode;
+};
+
+type DocumentProps = DocumentInitialProps & {
+  colorMode: MaybeColorMode;
+};
+
+export default class MyDocument extends Document<DocumentProps> {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     const styles = CssBaseline.flush();
+
+    let colorMode: MaybeColorMode = theme.config.initialColorMode;
+
+    if (ctx.req?.headers.cookie) {
+      colorMode =
+        parseCookie(ctx.req.headers.cookie, 'chakra-ui-color-mode') ||
+        theme.config.initialColorMode;
+    }
 
     return {
       ...initialProps,
@@ -27,16 +44,18 @@ export default class MyDocument extends Document {
           {styles}
         </Fragment>,
       ],
+      colorMode,
     };
   }
 
   render(): JSX.Element {
+    const { colorMode } = this.props;
+
     return (
-      <Html lang="en">
+      <Html lang="en" data-theme={colorMode} style={{ colorScheme: colorMode }}>
         <Head />
 
-        <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <body className={!colorMode ? undefined : `chakra-ui-${colorMode}`}>
           <Main />
           <NextScript />
         </body>
