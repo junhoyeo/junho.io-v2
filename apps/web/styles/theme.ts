edb63d9c03067f1ec679f3e771eb035ad2ddbad7
Junho Yeo <i@junho.io>
2022-12-17 23:06:52 +0900

    [web] Set Chakra's `initialColorMode` to `dark`

--- apps/web/styles/theme.ts
+++ apps/web/styles/theme.ts
@@ -1,6 +1,10 @@
 import { extendTheme, type Theme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
   textStyles: {
     h4: {
       letterSpacing: '0',
