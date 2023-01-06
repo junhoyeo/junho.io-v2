600f20e4e03d136a2550e983dd15a0f969abae48
Junho Yeo <i@junho.io>
2023-01-06 22:17:59 +0900

    [web] Detach `Header` component from `HomePage`

---
+++ apps/web/home/components/Header.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+import { useTheme } from '@geist-ui/core';
+
+export const Header: React.FC = () => {
+  const { palette } = useTheme();
+
+  return (
+    <Container>
+      <Title style={{ color: palette.accents_7 }}>
+        {`Hi, I'm `}
+        <span>Junho Yeo</span>
+      </Title>
+      <Description style={{ color: palette.accents_6 }}>
+        {`Welcome to my site. I'm a 19-yo generalist hacker, shaping the 2nd/3rd
+          web. Sometimes a designer and dreamer, I just like to build things.
+          Now I'm preparing the infrastructure—insight, followers, and
+          capital—for a bigger dream.`}
+      </Description>
+    </Container>
+  );
+};
+
+const Container = styled.header`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  margin-bottom: 28px;
+
+  font-weight: 800;
+  font-size: 2rem;
+  line-height: 1.25;
+  letter-spacing: -0.25px;
+`;
+const Description = styled.p`
+  margin: 0;
+  margin-bottom: 20px;
+
+  font-weight: 400;
+  font-size: 18px;
+  padding-bottom: 0.56rem;
+  line-height: 1.45;
+`;
