31aee676324be5473246704e952966b22d348e7b
Junho Yeo <i@junho.io>
2023-02-09 11:15:15 +0900

    [web] Add colors for ToC

--- apps/web/posts/components/ToC.tsx
+++ apps/web/posts/components/ToC.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { useTheme } from '@geist-ui/core';
 
 import { type Heading } from '../lib/rehype-extract-headings';
 
@@ -6,13 +7,18 @@ type ToCProps = {
   headings: Heading[];
 };
 export const ToC: React.FC<ToCProps> = ({ headings }) => {
+  const { palette } = useTheme();
+
   return (
     <Container id="toc">
       {headings.map((heading) => (
         <a
           key={heading.id}
           href={`#${heading.id}`}
-          style={{ marginLeft: 16 * (heading.rank - 2) }}
+          style={{
+            marginLeft: 16 * (heading.rank - 2),
+            fontWeight: 500,
+          }}
           onClick={(e) => {
             e.preventDefault();
             // smoothly scroll into `#${heading.id}` but with top offset of 82px. do not use scrollIntoView.
@@ -47,4 +53,13 @@ const Container = styled.div`
 
   width: 240px;
   min-width: 240px;
+
+  a {
+    color: rgb(102, 102, 102);
+    transition: color 0.1s ease-in-out;
+
+    &:hover {
+      color: rgb(187, 187, 187);
+    }
+  }
 `;
